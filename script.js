167db0124903e31cde77b4e40987679e44e5dd60
Devilal panchal <devilalpanchal551@gmail.com>
2024-04-03 21:16:43 +0530

    update api

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 
-
 const adviceid = document.querySelector('#advice')
-const  para = document.querySelector('#paragrafh')
-const  Randombtn = document.querySelector('.diceimage')
+const para = document.querySelector('#paragrafh')
+const Randombtn = document.querySelector('.diceimage')
 
 async function fetchData() {
     let url = 'https://api.adviceslip.com/advice'
 
     fetch(url).then((responce) => responce.json()).then((data) => {
-        adviceid.innerHTML =  data.slip.id
-        para.innerHTML =  data.slip.advice;
-        console.log(data);
-        console.log(data.slip.id);
-        console.log(data.slip.advice);
+        adviceid.innerHTML = `<p>ADVICE # &nbsp;${data.slip.id}</p>`
+        para.innerHTML = `<q>${data.slip.advice}</q> ` 
+        
     })
         .catch((err) => {
             let error = new Error();
@@ -20,3 +17,8 @@ async function fetchData() {
         })
 }
 fetchData()
+
+document.querySelector('.diceimage').addEventListener('click', () => {
+    document.location.reload()
+    fetchData()
+})
